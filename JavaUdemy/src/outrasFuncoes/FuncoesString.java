6c01dc6f909ad6231923054600cb0c3d79633819
rivadavia junior <rivajunior.rec@gmail.com>
2021-01-26 09:52:57 -0300

    Outras funções do String

---
+++ JavaUdemy/src/outrasFuncoes/FuncoesString.java
@@ -0,0 +1,30 @@
+package outrasFuncoes;
+
+public class FuncoesString {
+
+	// OUTRAS FUNCOES DO STRING
+	
+	public static void main(String[] args) {
+		
+		String original = "abcde ABC RSTU jlmno ";
+		
+		String s1 = original.toLowerCase();
+		String s2 = original.toUpperCase();
+		String s3 = original.trim();  // tira os espacos em branco
+		String s4 = original.substring(2); // gera uma novo String a partir do segundo caractere
+		String s5 = original.substring(0,10); // gera uma novo String a partir do caractere 0 ao 10
+		String s6 = original.replace("a","x");// troca o A por X
+		String s7 = original.replace("ABC","XYZ");
+		
+		System.out.println("Original: -" + original + "-");
+		System.out.println("toLowerCase: -"+ s1 + "-");
+		System.out.println("toUpperCase: -"+ s2 + "-");
+		System.out.println("trim: -"+ s3 + "-"); 
+		System.out.println("substring: - " + s4 + "-");
+		System.out.println("substring 0 ao 10: - " + s5 + "-"); 
+		System.out.println("Replace 1: - " + s6 + "-"); 
+		System.out.println("Replace 2: - " + s7 + "-");
+
+	}
+
+}
---
+++ JavaUdemy/src/outrasFuncoes/OperacaoSplit.java
@@ -0,0 +1,10 @@
+package outrasFuncoes;
+
+public class OperacaoSplit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
