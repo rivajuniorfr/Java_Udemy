80df4e31e204784687715e96bcca55cfa47f010d
rivadavia junior <rivajunior.rec@gmail.com>
2021-03-01 11:08:17 -0300

    criando metodos de operacoes

--- JavaUdemy/src/PooAplication/ProgramEstoque.java
+++ JavaUdemy/src/PooAplication/ProgramEstoque.java
@@ -1,8 +1,26 @@
 package PooAplication;
 
+import java.util.Scanner;
+
+import PooEntities.Produto;
+
 public class ProgramEstoque {
 
 	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner (System.in);
+		
+		Produto produto = new Produto();
+		
+		System.out.println("Digite os dados do Produto");
+		System.out.println("Nome: ");
+		produto.name= entrada.nextLine();
+		System.out.println("Preco: ");
+		produto.price = entrada.nextDouble();
+		System.out.println("Quantidade: ");
+		produto.quantity = entrada.nextInt();
+		
+		entrada.close();
 		
 
 	}
--- JavaUdemy/src/PooEntities/Produto.java
+++ JavaUdemy/src/PooEntities/Produto.java
@@ -2,8 +2,57 @@ package PooEntities;
 
 public class Produto {
 	
-	String name;
-	double price;
-	int quantity;
+	// Atributos
+	
+     public String name;
+	 public double price;
+	 public int quantity;
+	
+	
+	// Construtor
+	
+	public Produto() {
+		super();
+		
+	}
+	
+	// Getters and Setters
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	// Operacoes
+	
+	public double totalValueInStock () {
+		
+		return price * quantity;
+	}
+	
+	public void addProduct ( int quantity ) {
+		
+		this.quantity += quantity;
+	}
+	
+	public void removeProducts (int quantity) {
+		
+		this.quantity -= quantity;
+	}
 
 }
